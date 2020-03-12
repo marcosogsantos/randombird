23efd5ee2880f611d7926fb5f746568cf662f8f7
marcosogsantos <marcos_santos100@icloud.com>
2020-03-12 04:39:46 -0300

    Updates

---
+++ precache-manifest.acf96134b532dd30b0b58645484cf29c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91f5e369aee9644a83301ef27398528d",
+    "url": "/randombird/index.html"
+  },
+  {
+    "revision": "7f428be4621f84dd5f54",
+    "url": "/randombird/static/css/main.14342a82.chunk.css"
+  },
+  {
+    "revision": "5a0fb05c8c629824537f",
+    "url": "/randombird/static/js/2.11e76a0e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/randombird/static/js/2.11e76a0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f428be4621f84dd5f54",
+    "url": "/randombird/static/js/main.0ff31f81.chunk.js"
+  },
+  {
+    "revision": "bdec6dbf352574c67dec",
+    "url": "/randombird/static/js/runtime-main.83e95e3a.js"
+  }
+]);
